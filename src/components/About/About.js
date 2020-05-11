513b77e3787fe63cbfed9a677555bd6234be8f20
madu munachi <madustanley1@gmail.com>
2020-05-11 19:43:01 +0100

    portfolio

--- src/components/About/About.js
+++ src/components/About/About.js
@@ -11,13 +11,13 @@ class About extends React.Component {
                 <Div> 
                     <div className='div'>
                         <H1>ABOUT</H1><br />
-                    <H1>MADU STANLEY MUNACHISO</H1>
+                    <h1>MADU STANLEY MUNACHISO</H1>
                     </div><br />
                     <div>
                         <p>Madu Stanley Munachiso is a self taught Front-End Developer who lives in Owerri, Imo State, Nigeria. 
                             He has a Bachelor's degree in Urban and Regional Planning from the Federal University Of Technology
                             Owerri. He thought that he would end up to be a town planner who  design estates in a studio or 
-                            who review development plans in his office. Then he felt the spark that he needed to learn something 
+                            review development plans in his office. Then he felt the spark that he needed to learn something 
                             else.
                         </p>
                         <p>Then he was introduced to web development by his elder brother who is a developer. He just fell in love
